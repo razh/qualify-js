173e28f52699845f499e9cf82fcd9994cf60a35f
Raymond Zhou <somecrazyidiot@gmail.com>
2013-05-27 00:59:30 -0400

    Add HtmlReporterHelper (as QualifyHtmlReporter.View).

--- app/scripts/lib/QualifyHtmlReporter.js
+++ app/scripts/lib/QualifyHtmlReporter.js
@@ -17,7 +17,27 @@
     };
   };
 
+  // Abstract class for QualifyHtmlReporter view.
+  jasmine.QualifyHtmlReporter.View = function() {};
 
+  jasmine.QualifyHtmlReporter.View.prototype.createDOM = function() {};
+
+  jasmine.QualifyHtmlReporter.View.prototype.append = function( child, childElement ) {
+    var parent = child[ parentSuite ];
+  };
+
+  jasmine.QualifyHtmlReporter.View.prototype.getSpecStatus = function( child ) {
+    var results = child.results();
+
+    var status = results.passed() ? 'passed' : 'failed';
+    if ( results.skipped ) {
+      status = 'skipped';
+    }
+
+    return status;
+  };
+
+  // Reporter view.
   jasmine.QualifyHtmlReporter.ReporterView = function() {
     this.totalSpecCount = 0;
 
@@ -37,16 +57,26 @@
     return this;
   };
 
+  jasmine.QualifyHtmlReporter.ReporterView.prototype = new jasmine.QualifyHtmlReporter.View();
+  jasmine.QualifyHtmlReporter.ReporterView.prototype.constructor = jasmine.QualifyHtmlReporter.ReporterView;
 
+  // Suite view.
   jasmine.QualifyHtmlReporter.SuiteView = function( suite, views ) {
     this.suite = suite;
     this.views = views;
   };
 
+  jasmine.QualifyHtmlReporter.SuiteView.prototype = new jasmine.QualifyHtmlReporter.View();
+  jasmine.QualifyHtmlReporter.SuiteView.prototype.constructor = jasmine.QualifyHtmlReporter.SuiteView;
 
+
+  // Spec view.
   jasmine.QualifyHtmlReporter.SpecView = function( spec, views ) {
     this.spec = spec;
     this.views = views;
   };
+
+  jasmine.QualifyHtmlReporter.SpecView.prototype = new jasmine.QualifyHtmlReporter.View();
+  jasmine.QualifyHtmlReporter.SpecView.prototype.constructor = jasmine.QualifyHtmlReporter.SpecView;
 
 }) ( document, jasmine );
