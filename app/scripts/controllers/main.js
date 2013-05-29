57e77053f21dd4deaa534cec961b29047f34ca6b
Raymond Zhou <somecrazyidiot@gmail.com>
2013-05-29 00:10:48 -0400

    SimpleReporter now appears to work. Example Jasmine tests are now displayed.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -44,7 +44,7 @@ angular.module( 'qualifyJsApp' )
       '    publicProperty: null,\n' +
       '    publicMethod: function() {\n' +
       '      // Private properties and methods can be accessed from here.\n' +
-      '      console.log( privateProperty );\n' +
+      '      console.log(privateProperty);\n' +
       '    }\n' +
       '  };\n' +
       '}) ();';
@@ -57,22 +57,41 @@ angular.module( 'qualifyJsApp' )
       console.log( Object.keys( eval( $scope.code ) ) );
     };
 
-
-    var reporter = new jasmine.SimpleReporter();
-
     // Test definition code.
-    var jasmineEnv = jasmine.getEnv();
-    jasmineEnv.updateInterval = 1000;
-
-    describe( 'Test', function() {
-      it( 'should add two numbers correctly', function() {
-        expect( 2 + 2 ).toBe(4);
-      });
+    var reporter = new jasmine.SimpleReporter();
+    reporter.onRunnerFinished(function( output ) {
+      $scope.output = output;
+      $scope.$apply();
     });
+
+    var jasmineEnv = jasmine.getEnv();
+
+    jasmineEnv.updateInterval = 1000;
+    jasmineEnv.addReporter( reporter );
 
     $scope.suites = [{specs: [{name: 'hello'}]}, {specs: [{name: 'goodbye'}]}];
 
+    var number = 2;
+    var nullObject = null;
     $scope.testCode = function() {
+      describe( 'Test', function() {
+        it( 'should add two numbers correctly', function() {
+          console.log('first');
+          expect( number + number ).toBe( 2 * number );
+        });
+
+        it( 'should have an object with a value of null', function() {
+          console.log('second');
+
+          expect( nullObject ).toBeNull();
+        });
+      });
+
       jasmineEnv.execute();
+
+      setTimeout(function() {
+        number++;
+        nullObject = {};
+      }, jasmine.updateInterval );
     };
   }]);
--- app/scripts/lib/SimpleReporter.js
+++ app/scripts/lib/SimpleReporter.js
@@ -1,22 +1,36 @@
 jasmine.SimpleReporter = function() {
+  jasmine.Reporter.call( this );
 
+  this.output = [];
+  this.callbackEnd = null;
+};
+
+jasmine.SimpleReporter.prototype = new jasmine.Reporter();
+jasmine.SimpleReporter.prototype.constructor = jasmine.SimpleReporter;
+
+jasmine.SimpleReporter.prototype.onRunnerFinished = function( callback ) {
+  this.callbackEnd = callback;
 };
 
 jasmine.SimpleReporter.prototype.reportRunnerStarting = function( runner ) {
+  this.output = [];
 };
 
 jasmine.SimpleReporter.prototype.reportRunnerResults = function( runner ) {
+  var result = runner.results();
+
+  if ( this.callbackEnd ) {
+    this.callbackEnd( this.output );
+  }
 };
 
-jasmine.SimpleReporter.prototype.reportSuiteResults = function( suite ) {
-};
-
-jasmine.SimpleReporter.prototype.reportSpecStarting = function( spec ) {
-};
+jasmine.SimpleReporter.prototype.reportSuiteResults = function( suite ) {};
+jasmine.SimpleReporter.prototype.reportSpecStarting = function( spec ) {};
 
 jasmine.SimpleReporter.prototype.reportSpecResults = function( spec ) {
+  var result = spec.results();
+
+  this.output.push( spec.description + ': ' + ( result.passed() ? 'passed': 'failed' ) );
 };
 
-jasmine.SimpleReporter.prototype.log = function( str ) {
-};
-
+jasmine.SimpleReporter.prototype.log = function( str ) {};
