cd1b8123dd91cfbaa8a8361a9bde191e68b7c8d1
Raymond Zhou <somecrazyidiot@gmail.com>
2013-06-12 13:56:08 -0400

    Specs view now displays specs.

--- app/scripts/controllers/specs.js
+++ app/scripts/controllers/specs.js
@@ -4,28 +4,44 @@ angular.module( 'qualifyJsApp' )
   .controller( 'SpecsCtrl',
     [ '$scope',
       'jasmineService', function( $scope, jasmineService ) {
-    $scope.specs = [];
+    $scope.suites = [];
 
     // Load methods and vars from jasmine service.
     var jasmineEnv = jasmineService.env,
         resetJasmineRunner = jasmineService.resetJasmineRunner,
         reporter = jasmineService.reporter;
 
+    // Whenever the selected problem changes, update the specs view.
     $scope.$watch( 'selected.problem', function( newValue ) {
       if ( !newValue.suite ) {
         return;
       }
 
+      // Variables available to test suite.
+      var $code, $results;
       // Execute the suites once so we can grab spec data.
       eval( newValue.suite.join( '\n' ) );
 
       var suites = jasmineEnv.currentRunner().suites();
-      suites.forEach(function() {
+      var specs, tempSuite;
 
+      // Reset specs array.
+      $scope.suites = [];
+      suites.forEach( function( suite ) {
+        tempSuite = [];
+
+        specs = suite.specs();
+        specs.forEach( function( spec ) {
+          tempSuite.push( spec.description );
+        });
+
+        $scope.suites.push( tempSuite );
       });
 
       // Remove onRunnerFinished handler from reporter.
       reporter.onRunnerFinished( null );
+      // Executing and then reseting the jasmine runner prevents duplicate alerts
+      // from popping up.
       jasmineEnv.execute();
       resetJasmineRunner( jasmineEnv.currentRunner() );
     });
